7a0386017c0666ca2df8b80d1f48f3337c57992e
aesher9o1 <aashiskumar986@gmail.com>
2019-11-15 10:21:40 +0530

    FIX email going to wrong participants

--- routes/mailing.js
+++ routes/mailing.js
@@ -2,23 +2,25 @@
 import router from 'express'
 import nodemailer from 'nodemailer'
 import { PASSWORD } from '../environment/secrets'
-import xoauth2 from 'xoauth2'
-let route = router()
-route.post('/', async (req, res) => {
-    console.log(PASSWORD)
-    var smtpTransport = nodemailer.createTransport("smtps://aashis.spam%40gmail.com:" + encodeURIComponent(PASSWORD) + "@smtp.gmail.com:465");
+import emailTempate from './emailTemplate'
 
+let route = router()
+const smtpTransport = nodemailer.createTransport("smtps://aashis.spam%40gmail.com:" + encodeURIComponent(PASSWORD) + "@smtp.gmail.com:465");
+
+route.post('/', async (req, res) => {
+    console.log(req.body)
     var mailOptions = {
         from: "aashis.spam@gmail.com",
-        to: "aashiskumar986@gmail.com",
-        subject: ' | new message !',
-        text: "hey"
+        to: req.body.requestedBy.requestedBy,
+        subject: req.body["subject"],
+        cc: (typeof req.body.participants == 'string') ? req.body.participants : req.body.participants,
+        html: emailTempate(req.body["body"])
     }
     smtpTransport.sendMail(mailOptions, function (error, response) {
         if (error) {
-            console.log(error);
+            res.error(error)
         } else {
-            res.send("done");
+            res.send({ "message": "DELIVERED" });
         }
     });
 })
